dad2ddf058de9ca5b65e92c9cbb5585c4f2f1934
ArvikVan <arvikvan@gmail.com>
2021-03-01 23:24:31 +0300

    2. Сокобан. Тестовое задание. [#475734]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,42 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if ((board[i][i] == 1) && (monoVertical(board, i) || monoHorizontal(board, i))) {
+                return monoVertical(board, i) || monoHorizontal(board, i);
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoVertical(int[][] board, int column) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static int[] extractDiagonal(int[][] board) {
+        int[] rsl = new int[board.length];
+        for (int i = 0; i < board.length; i++) {
+            rsl[i] = board[i][i];
+        }
         return rsl;
     }
 }
--- puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
+++ puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
@@ -55,4 +55,16 @@ public class WinTest {
         };
         assertThat(Win.check(board), is(false));
     }
+
+    @Test
+    public void whenVerticalWinTo() {
+        int[][] board = {
+                {1, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+        };
+        assertThat(Win.check(board), is(true));
+    }
 }
